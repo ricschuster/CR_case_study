3a4e4b919d0c63f652b096f123cf292acf16df79
RicSchuster <mail@richard-schuster.com>
2019-10-07 15:20:44 -0700

    progress

--- CR_zones_LAB_rs_2.R
+++ CR_zones_LAB_rs_2.R
@@ -19,8 +19,11 @@ library("rgdal")
 #install.packages("slam")
 library("slam")
 library("gurobi")
+#install.packages("here")
+library("here")
 
-library(here)
+library(doParallel)
+
 ###################################################################
 #sTEP TWO: Set work directory, which should be where the data is saved
 ## Set work directory and import data
@@ -32,10 +35,11 @@ raster_files <- list.files(datfolder, pattern=".tif$",
 raster_files
 
 #import all raster files in folder using lapply
+
 allrasters <- stack(raster_files)
 
 #to check the index numbers of all imported raster list elements
-allrasters[[1]]
+allrasters
 
 #call single raster element
 allrasters[[80]]
@@ -55,9 +59,9 @@ names(species)
 #Ecosystem Services:
 Biodiversity <- raster("Biodiversity Intactness Index.tif") #Reads raster layers
 import_hidrica<- raster("Water Importance.tif")
-carbon <- raster("Geo Carbon.tif")
-organic_s <- raster("Organic Soil.tif")
-water_q<- raster("Quality of the Residual Surface Water.tif")
+carbon <- raster("Geo Carbon.tif") / 1000000
+organic_s <- raster("Organic Soil.tif") / 1000000
+water_q<- raster("Quality of the Residual Surface Water.tif") / 10000
 agricutlrure <- raster("Agriculture Suitability.tif")
 #mangrove<- raster("C:/Users/Jenny/Dropbox/ELSA pilot - Costa Rica/input data original/hamilton_mangroves2014_cri.tif")
 Mangroves<- raster("Mangroves.tif")
@@ -114,6 +118,7 @@ carbon3<-carbon*0.5
 organic_s3 <- organic_s*0.75
 water_q3<- water_q*0.75
 Mangroves3<- Mangroves*0
+species3<-species*0.5
 
 
 #Setting up the cost layer: We can either set a cost as land, so prioritizR
@@ -125,92 +130,173 @@ Mangroves3<- Mangroves*0
 PU<-(PU/PU) #In this case, cost is the same across the landscape
 #Create zonal layers
 HFP_in<-HFP<4
-plot(HFP_in)
+plot(HFP_in,add=TRUE)
 HFP_res<-HFP>4&HFP<20
 plot(HFP_res)
-HFP_mg<-HFP>20
+HFP_mg<-HFP<20
 plot(HFP_mg)
+HFP_BAU<-HFP>=20
+plot(HFP_BAU)
 
 cellStats(PU,"sum")
 
-## Creating budgets based on the total value in each cost layer
-budget<-c(53290,53290,53290)
 
 ##Create planning units stack with the cost layers
 
-pu1 <- stack(PU, PU,PU)
-names(pu1) <- c("zone_1", "zone_2", "zone_3")
+pu1 <- stack(PU, PU,PU,PU)
+names(pu1) <- c("zone_1", "zone_2", "zone_3", "zone_4")
 
 ##I included in each zone, all three zone layers (low, medium and high HFP)
 
 zn1<-stack(Biodiversity,import_hidrica,carbon ,organic_s ,
            water_q,agricutlrure1 ,Mangroves, wetlands,bio_corridors,
-           PA,forest_reserv,T_indigena,HFP_in,HFP_res*0,HFP_mg*0)
+           # PA,forest_reserv,T_indigena,
+           HFP_in,HFP_res*0,HFP_mg*0,HFP_BAU*0)
 zn2<-stack(Biodiversity2,import_hidrica2,carbon2 ,organic_s ,
            water_q2,agricutlrure1 ,Mangroves, wetlands2,bio_corridors2,
-           PA2,forest_reserv2,T_indigena2,HFP_in*0,HFP_res,HFP_mg*0)
+           # PA2,forest_reserv2,T_indigena2,
+           HFP_in*0,HFP_res,HFP_mg*0,HFP_BAU*0)
 zn3<-stack(Biodiversity3,import_hidrica3,carbon3 ,organic_s3 ,
            water_q3,agricutlrure ,Mangroves3, wetlands3,bio_corridors3,
-           PA3,forest_reserv3,T_indigena3,HFP_in*0,HFP_res*0,HFP_mg)
-
+           # PA3,forest_reserv3,T_indigena3,
+           HFP_in*0,HFP_res*0,HFP_mg,HFP_BAU*0)
+zn4<-stack(Biodiversity3*0,import_hidrica3*0,carbon3*0 ,organic_s3*0 ,
+           water_q3*0,agricutlrure ,Mangroves3*0, wetlands3*0,bio_corridors3*0,
+           # PA3*0,forest_reserv3*0,T_indigena3*0,
+           HFP_in*0,HFP_res*0,HFP_mg,HFP_BAU)
 
 ### Create Zone file
 
-z2 <- zones("zone_1" = zn1, "zone_2" = zn2,  "zone_3" = zn3)
+z2 <- zones("zone_1" = zn1, "zone_2" = zn2,  "zone_3" = zn3, "zone_4" = zn4)
 
 
 ## Setting overall targets:
-
+# 
 t4 <- tibble::tibble(feature = names(zn1),
-                     zone = list(names(pu1))[rep(1, 15)],
-                     target = c(rep(0.1, 12), 0.3, 0.2, 0.3),
-                     type = rep("relative", 15))
-##Problem
+                     zone = list(names(pu1))[rep(1, 13)],
+                     target = c(rep(0.2, 9), 0.3, 0.2, 0.2,0.3),
+                     type = rep("relative", 13))
 
-p4 <- problem(pu1, zones("zone_1" = zn1, "zone_2" = zn2, 
-                         "zone_3" = zn3,
-                         feature_names = names(zn1))) %>%
-  add_boundary_penalties(penalty = 0.00001) %>%
-  add_min_set_objective() %>%
-  add_manual_targets(t4) %>%
-  add_binary_decisions()
-
-s<-solve(p4, force=TRUE) # It was giving me a message saying: Warning in presolve_check.OptimizationProblem(compile(x)) :
-                         #features target values are (relatively) very high
-
-plot(category_layer(s), main="solution")
-fr <- feature_representation(p4, s)
-fr[1:15,]
+t4
+# ##Problem
+# 
+# p4 <- problem(pu1, zones("zone_1" = zn1, "zone_2" = zn2, 
+#                          "zone_3" = zn3,"zone_4" = zn4,
+#                          feature_names = names(zn1))) %>%
+#   add_min_set_objective() %>%
+#   add_manual_targets(t4) %>%
+#   add_binary_decisions()
+# 
+# s<-solve(p4, force=TRUE) # It was giving me a message saying: Warning in presolve_check.OptimizationProblem(compile(x)) :
+#                          #features target values are (relatively) very high
+# 
+# plot(category_layer(s), main="solution")
+# fr <- feature_representation(p4, s)
+# fr[15:35,]
 
 
 ## Matrix for clumping rules.
 # print stack
-print(pu1)
-plot(pu1)
+# print(pu1)
+# plot(pu1)
+# 
+# z6 <- diag(4)
+# z6[1, 2] <- 1
+# z6[2, 1] <- 1
+# z6[2, 3] <- 1
+# z6[3, 2] <- 1
+# 
+# z6
+# colnames(z6) <- c("zone_1", "zone_2", "zone_3", "zone_4")
+# rownames(z6) <- colnames(z6)
 
-z6 <- diag(3)
-z6[1, 2] <- 1
-z6[2, 1] <- 1
-z6[2, 3] <- 1
-z6[3, 2] <- 1
-
-z6
-colnames(z6) <- c("zone_1", "zone_2", "zone_3")
-rownames(z6) <- colnames(z6)
-
-
+# parallelization
+n_cores <- 12
+cl <- makeCluster(n_cores)
+registerDoParallel(cl)
 
 p1 <-   problem(pu1, zones("zone_1" = zn1, "zone_2" = zn2, 
-                           "zone_3" = zn3,
+                           "zone_3" = zn3,"zone_4" = zn4,
                            feature_names = names(zn1))) %>%
   add_min_set_objective() %>%
   add_manual_targets(t4) %>%
-  add_binary_decisions()
-  add_boundary_penalties(penalty=0.00001, zones = z6)%>%
-  add_gurobi_solver(gap=10)
+  add_binary_decisions()%>%
+  # add_proportion_decisions %>%
+  # add_boundary_penalties(penalty = 0.0000001)%>%
+  add_gurobi_solver(threads = n_cores)
 
-s1 <- solve(p1, run_checks=FALSE, force=TRUE)
+s1 <- solve(p1, force=TRUE)
 setMinMax(s1)
-plot(category_layer(s), main="solution")
+plot(category_layer(s1), main="solution")
 
 fr <- feature_representation(p1, s1)
+
+fr[50:64,]
+
+####################################################################
+### Loop to run multiple scenarios of feature representation where
+### zones representation of low, medium and high HFP is the same
+z2<-zones("zone_1" = zn1, "zone_2" = zn2, 
+      "zone_3" = zn3,"zone_4" = zn4,
+      feature_names = names(zn1))
+
+result_list <- list()
+for(ii in 1:100){
+  
+  t4 <- tibble::tibble(feature = names(zn1),
+                       zone = list(names(pu1))[rep(1, 16)],
+                       target = c(rep((0.01*ii), 12), 0.3, 0.2, 0.2,0.3),
+                       type = rep("relative", 16))
+  
+  p2<- problem(pu1, z2) %>%
+        add_min_set_objective() %>%
+        add_manual_targets(t4) %>%
+        add_binary_decisions()%>%
+        add_boundary_penalties(0.0001)%>%
+        add_gurobi_solver(gap=0.1)
+                                 
+  s2 <- solve(p2,force=TRUE)
+  
+  result_list[[ii]] <- s2 
+  fr <- feature_representation(p2,s2) 
+  pu<- cellStats(s2, "sum") 
+  
+  if(ii == 1){
+    feat_rep_rel <- data.frame(rbind(c(fr$relative_held)))
+    feat_rep_abs <- data.frame(rbind(c(fr$absolute_held)))
+    planning_u <- data.frame(rbind(c(pu)))
+    names(feat_rep_rel) <- fr$feature
+    names(feat_rep_abs) <- fr$feature
+    names(planning_u)<- c("PU")
+  } else {
+    feat_rep_rel[ii,] <- rbind(c(fr$relative_held))
+    feat_rep_abs[ii,] <- rbind(c(fr$absolute_held))
+    planning_u[ii,] <- rbind(c(pu))
+  }
+  plot(category(s2), main=(paste("Scenario",ii))) 
+  print(paste("End of scenario",ii))
+  rm(p2, s2, fr,pu)
+  
+  
+}
+
+feat_rep_abs$scenario<- c(1:(nrow(feat_rep_abs)))
+feat_rep_rel$scenario<- c(1:(nrow(feat_rep_abs)))
+planning_u$scenario<- c(1:(nrow(feat_rep_abs)))
+
+CR_Toff<- full_join(feat_rep_rel,feat_rep_abs,by="scenario")
+CR_Toff<- full_join(CR_Toff,planning_u,by="scenario")
+
+write.csv(CR_Toff,"CR_Toff.csv")
+CR_Toff
+
+CR_Toff_stack <- stack(result_list)
+plot(category_layer(CR_Toff_stack[[1:4]]))
+
+writeRaster(CR_Toff_stack,"CR_Toff_stack.tif", overwrite=TRUE)
+
+                         
+                         
+
+# clean up
+stopCluster(cl)
